e3f4f3bd6b96e75ed9aa6fb004703783006fd3a0
tsunodayuki0302 <tsunoda.yuki0302@gmail.com>
2021-09-23 12:18:37 +0900

    エラーの修正。一回目。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
       redirect_to prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
+      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:show, :edit]
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy, :show]
 
 
   def index
